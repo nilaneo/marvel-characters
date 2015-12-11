ee9c4a0db52fe287c32dcd7dd61017c9139bf79f
Neonila Shlikhtenko <n.shlikhtenko@levi9.com>
2015-12-11 15:31:40 +0200

    Use controller as syntax

--- app/marvel-characters.start.js
+++ app/marvel-characters.start.js
@@ -3,17 +3,22 @@
 	
 	var app = angular.module('MarvelCharacters', []);
 
-	app.controller('charactersCtrl', function($scope, $http) {
+	app.controller('CharactersCtrl', function($scope, $http) {
+		var charactersCtrl = this;
+
 		$http.get("http://gateway.marvel.com/v1/public/characters?orderBy=-modified&apikey=e82e1f8eb16da85c0260676f2cdb05b2")
 	    .then(function(response) {
-	    	$scope.characters = response.data.data.results;
+	    	charactersCtrl.characters = response.data.data.results;
 	    });
 	});
 
-	app.directive('marvelCharacter', function() {
+	app.directive('marvelCharacters', function() {
 		return {
 			templateUrl: "app/characters.tmpl.html",
-			restrict: "E"
+			restrict: "E",
+			scope: {
+				characters: "="
+			}
 		}
 	});
 }());
