b659b47ee4d7d9bc19f762f2f57988719c81f63e
Neonila Shlikhtenko <n.shlikhtenko@levi9.com>
2015-12-11 17:41:36 +0200

    Use ngRoute

--- app/marvel-characters-app.js
+++ app/marvel-characters-app.js
@@ -1,24 +1,26 @@
 (function () {
 	"use strict";
 	
-	var app = angular.module('MarvelCharactersApp', []);
+	var app = angular.module('MarvelCharactersApp', [
+		'ngRoute'
+	]);
+
+	app.config(function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				controller: 'MarvelCharactersAppCtrl',
+				controllerAs: 'vm',
+				templateUrl: 'app/marvel-characters-app.html'
+			})
+			.otherwise('/');
+	});
 
 	app.controller('MarvelCharactersAppCtrl', function($http) {
 		var vm = this;
 
 		$http.get("http://gateway.marvel.com/v1/public/characters?orderBy=-modified&apikey=e82e1f8eb16da85c0260676f2cdb05b2")
-	    .then(function(response) {
-	    	vm.characters = response.data.data.results;
-	    });
-	});
-
-	app.directive('marvelCharactersApp', function() {
-		return {
-			templateUrl: "app/marvel-characters-app.html",
-			restrict: "E",
-			scope: {},
-			controller: 'MarvelCharactersAppCtrl',
-			controllerAs: 'vm'
-		}
+		    .then(function(response) {
+		    	vm.characters = response.data.data.results;
+		    });
 	});
 }());
