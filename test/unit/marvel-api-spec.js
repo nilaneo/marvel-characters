b85d6da6259673084047283e6d3e683471b1e3e0
Neonila Shlikhtenko <n.shlikhtenko@levi9.com>
2016-01-15 19:49:01 +0200

    Finish tests for marvel-api service

--- test/unit/marvel-api-spec.js
+++ test/unit/marvel-api-spec.js
@@ -1,14 +1,17 @@
 describe('mocking service http call', function() {
     var $httpBackend;
     var $httpParamSerializer;
+    var $rootScope;
     var marvelApi;
+    var result;
 
     beforeEach(module('common.services.marvelApi'));
 
-    beforeEach(inject(function(_$httpBackend_, _$httpParamSerializer_, _marvelApi_){
+    beforeEach(inject(function(_$httpBackend_, _$httpParamSerializer_, _marvelApi_, _$rootScope_){
         marvelApi = _marvelApi_;
         $httpBackend = _$httpBackend_;
         $httpParamSerializer = _$httpParamSerializer_;
+        $rootScope = _$rootScope_;
     }));
 
     afterEach(function() {
@@ -17,20 +20,101 @@ describe('mocking service http call', function() {
     });
 
     describe('getCharacters', function(){
-        it('should call Marvel API with correct params', function() {
-            $httpBackend
-                .expectGET('http://gateway.marvel.com/v1/public/characters?' + $httpParamSerializer({
-                    apikey: 'e82e1f8eb16da85c0260676f2cdb05b2',
-                    limit: 20,
-                    nameStartsWith: 'ironman',
-                    offset: 40,
-                    orderBy: 'name'
-                }))
-                .respond({character: 'ironman'});
+        describe('when name is not empty', function() {
+            beforeEach(function() {
+                $httpBackend
+                    .expectGET('http://gateway.marvel.com/v1/public/characters?' + $httpParamSerializer({
+                        apikey: 'e82e1f8eb16da85c0260676f2cdb05b2',
+                        limit: 20,
+                        nameStartsWith: 'ironman',
+                        offset: 40,
+                        orderBy: 'name'
+                    }))
+                    .respond({
+                        data: {
+                            results: [
+                                {
+                                    character: 'ironman'
+                                }
+                            ]
+                        }
+                    });
 
-            marvelApi.getCharacters('ironman', 20, 3, 'name');
+                result = marvelApi.getCharacters('ironman', 20, 3, 'name');
+
+                $httpBackend.flush();
+            });
+
+            it('should call Marvel API with correct params', angular.noop);
+
+            it('should be resolved with an object with results', function(){
+                result.then(function(resolvedValue) {
+                    expect(resolvedValue).toEqual({
+                        results: [
+                            {
+                                character: 'ironman'
+                            }
+                        ]
+                    });
+                });
+            });
+        });
+
+        describe('when name is empty', function() {
+            beforeEach(function() {
+                $httpBackend
+                    .expectGET('http://gateway.marvel.com/v1/public/characters?' + $httpParamSerializer({
+                        apikey: 'e82e1f8eb16da85c0260676f2cdb05b2',
+                        limit: 20,
+                        offset: 40,
+                        orderBy: 'name'
+                    }))
+                    .respond({
+                        data: {
+                            results: [
+                                {
+                                    character: 'ironman'
+                                }
+                            ]
+                        }
+                    });
+
+                result = marvelApi.getCharacters('', 20, 3, 'name');
+
+                $httpBackend.flush();
+            });
+
+            it('should call Marvel API without nameStartsWith param', angular.noop);
+        });
+    });
+
+    describe('getCharacter', function(){
+        beforeEach(function () {
+            $httpBackend
+                .expectGET('http://gateway.marvel.com/v1/public/characters/25?' + $httpParamSerializer({
+                    apikey: 'e82e1f8eb16da85c0260676f2cdb05b2'
+                }))
+                .respond({
+                    data: {
+                        results: [
+                            {
+                                character: 'ironman'
+                            }
+                        ]
+                    }
+                });
+
+            result = marvelApi.getCharacter(25);
 
             $httpBackend.flush();
+        });
+
+        it('should call Marvel API with correct params to get one certain character', angular.noop);
+
+        it('should be resolved with an object with results', function(){
+            result.then(function(resolvedValue) {
+                expect(resolvedValue).toEqual({character: 'ironman'});
+            });
         });
     });
 });
